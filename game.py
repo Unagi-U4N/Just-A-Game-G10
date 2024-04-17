a37e22de14b5fb9e2d94e698944a5b87c5180647
Ivan Chong <1221109063@soffice.mmu.edu.my>
2024-04-17 13:49:02 +0800

    Added resources and game.py

---
+++ game.py
@@ -0,0 +1,6 @@
+# This part will be the main game loop and the main game logic
+# Must be updated frequently to keep the game running smoothly
+# Feel free to pull or inform me if you want to test our features from your branch
+
+# Author: Ivan
+
