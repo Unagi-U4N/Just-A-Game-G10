b51f380fdf1c87227694bbc9974d7331da0ae986
Ivan Chong <1221109063@soffice.mmu.edu.my>
2024-04-17 22:47:18 +0800

    Updated game.py, added basic movements

--- game.py
+++ game.py
@@ -2,4 +2,55 @@
 # Must be updated frequently to keep the game running smoothly
 # Feel free to pull or inform me if you want to test our features from your branch
 
-# Author: Ivan
+# Contributors: Ivan, Yuven, Putra
+
+import pygame, sys
+from utils import *
+from entities import PhysicsEntity
+
+class Game:
+    def __init__(self):
+        pygame.init()
+
+        pygame.display.set_caption('Just A Game')
+        self.screen = pygame.display.set_mode((1200, 675))
+        self.display = pygame.Surface((400, 225))
+        self.clock = pygame.time.Clock()
+
+        self.movements = [False, False]
+
+        self.assets= {
+            "player": load_image("entities/player.png")
+        }
+
+        self.player = PhysicsEntity(self, "player", (50, 50), (8, 15))
+        
+
+    def run(self):
+        while True:
+            
+            self.display.fill((255,255,255))
+
+            self.player.update((self.movements[1] - self.movements[0], 0))
+            self.player.render(self.display)
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_LEFT:
+                        self.movements[0] = True
+                    if event.key == pygame.K_RIGHT:
+                        self.movements[1] = True
+                if event.type == pygame.KEYUP:
+                    if event.key == pygame.K_LEFT:
+                        self.movements[0] = False
+                    if event.key == pygame.K_RIGHT:
+                        self.movements[1] = False
+
+            self.screen.blit(pygame.transform.scale(self.display, self.screen.get_size()),(0, 0))
+            pygame.display.update()
+            self.clock.tick(60)
+
+Game().run()
