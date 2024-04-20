dfca2997b1eada88121b7c4d1466bf37dddea577
Ivan Chong <1221109063@soffice.mmu.edu.my>
2024-04-20 21:54:14 +0800

    changed startscreen text

--- startscreen.py
+++ startscreen.py
@@ -7,20 +7,35 @@ class StartScreen:
         self.display = game.display
         self.clock = game.clock
         self.bg = game.assets["background"]
-        # self.bg = pygame.transform.scale(self.bg, (400, 225))
+        self.width = 1200
+        self.height = 675
         self.enter = False
+        self.transparency = 0
 
     def draw_text(self, text, font_name, color, x, y, size):
+        rgbt = list(color)
+        rgbt.append(self.transparency)
+        color = tuple(rgbt)
         font = pygame.font.SysFont(font_name, size)
-        text_surface = font.render(text, True, color)
+        text_surface = font.render(text, True, rgbt)
+        
+
+        # Creates a rectangle around the text surface, get the center of the rect
         text_rect = text_surface.get_rect()
         text_rect.center = (x, y)
         self.display.blit(text_surface, text_rect)
         pygame.display.update()
+    
+    def update(self):
+        if self.transparency == 255:
+            self.transparency = 0
+        else:
+            self.transparency += 5
 
     def run(self):
         self.display.blit(self.bg, (0, 0))
-        self.draw_text("Press SPACE to enter", "arialblack", (0, 0, 0), 20, 20, 50)  # Adjust the position of the text
+        self.update()
+        self.draw_text("Press SPACE to enter", "arialblack", (0, 0, 0), self.width/2, 550, 40)  # Adjust the position of the text
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
