f11cc2862c9c7b7261e3ebaae4f3d6536665a697
Ivan Chong <1221109063@soffice.mmu.edu.my>
2024-05-08 08:17:31 +0800

    changed state

--- game.py
+++ game.py
@@ -93,7 +93,7 @@ class Game:
         self.startscreen = StartScreen(self)
         self.game = Play(self)
         self.profile = PlayerProfile(self)
-        self.state = "game"
+        self.state = "start"
         self.cutscene = "Intro"
 
     def run(self):
