f6e73d86d6d221dd760a7255b467e991b6a36337
Ivan Chong <1221109063@soffice.mmu.edu.my>
2024-05-29 08:35:05 +0800

    edited levels

--- game.py
+++ game.py
@@ -22,7 +22,7 @@ class Game:
         self.clock = pygame.time.Clock()
         self.loaded = False
         self.particles = []
-        self.data = ["Ivan", "test", 10000, 3, 3]
+        self.data = ["Ivan", "2", 10000, 3, 3]
         # self.data = []
         self.sparks = []    
         self.projectiles = []
