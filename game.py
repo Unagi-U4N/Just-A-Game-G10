d01b042927ffdede06577a7b4ab7c3f27e4761bd
Ivan Chong <1221109063@soffice.mmu.edu.my>
2024-06-04 12:13:11 +0800

    added lock level algorithm

--- game.py
+++ game.py
@@ -22,7 +22,7 @@ class Game:
         self.clock = pygame.time.Clock()
         self.loaded = False
         self.particles = []
-        self.data = ["Ivan", "test3", 10000, 2.5, 3, 100]
+        self.data = ["Ivan", "2", 10000, 4, 3, 100]
         # self.data = []
         self.sparks = []    
         self.projectiles = []
--- play.py
+++ play.py
@@ -15,6 +15,7 @@ import dialogue
 from music import Music
 from ttt import *
 from playerprofile import *
+from safehouse import *
 
 class Play():
     def __init__(self, game):
@@ -48,10 +49,13 @@ class Play():
         self.font = game.font
         self.bg = self.assets["day"]
         self.sfx = game.sfx
-        self.level = 1
+        self.start = False
+        self.level = "1"
+        self.prevlevel = ""
         self.current_level = 0
         self.upgrade_choice = 0
         self.store = False
+        self.can_load_level = False
         self.level_select = False
         self.level_select_change = True
         self.store_state = "store"
@@ -108,7 +112,11 @@ class Play():
 
     def load(self, data):
         self.player.updateprofile(data)
-        self.level = str(data[1])
+
+        self.level = str(data[1]) if data[1] == "1" else "safehouse"
+        if self.level == "safehouse":
+            self.state = "safehouse"
+            self.start = True
         self.maxHP = self.player.HP
         self.lives = self.player.HP
         self.speed = self.player.speed
@@ -554,10 +562,11 @@ class Play():
                         self.level = "level_" + str(int(self.level.split("_")[1]) + 1)
                         self.level_select_change = False
 
-                    if event.key == pygame.K_SPACE:
+                    if event.key == pygame.K_SPACE and self.can_load_level:
                         self.level = self.level.split("_")[1]
                         self.load_level(self.level)
                         self.state = "game"
+                        self.start = False
                         self.level_select = False
 
         elif self.store:
@@ -782,140 +791,13 @@ class Play():
                 sys.exit()
                 
     def safehouse(self):
-        if self.state == "safehouse":
-            self.lives = self.player.HP
-            self.profile.data = self.player.data
-            self.profile.saveprogress()
-            
-            if self.savetimer < 100:
-                self.transitioning = True
-                self.savetimer += 1
-                self.display.fill((0, 0, 0))
-                self.display.blit(self.assets["save"], (0, 0))
-    
-            else:
-                self.transitioning = False
-                if self.level == "safehouse":
-                    self.level_transition(150, "Welcome to the safehouse")
-                if self.store:
-                    img = pygame.Surface((1200, 675))
-                    img.fill((0,0,0))
-                    img.set_alpha(200)
-                    self.display.blit(img, (0,0))
-                    self.display.blit(self.assets[self.store_state], (0, 0))
-                    if self.store_state == "store_menu":
-                        if self.upgrade_choice == 1:
-                            render_img(self.assets["speed_potion"], 445, 370, self.display, True)
-                            render_img(self.assets["big-heart"], 600, 370, self.display, True, transparency=150)
-                            render_img(self.assets["big-shield"], 750, 370, self.display, True, transparency=150)
-                        
-                        elif self.upgrade_choice == 0:
-                            render_img(self.assets["speed_potion"], 445, 370, self.display, True, transparency=150)
-                            render_img(self.assets["big-heart"], 600, 370, self.display, True)
-                            render_img(self.assets["big-shield"], 750, 370, self.display, True, transparency=150)
+       safehouse(self)
 
-                        elif self.upgrade_choice == 2:
-                            render_img(self.assets["speed_potion"], 445, 370, self.display, True, transparency=150)
-                            render_img(self.assets["big-heart"], 600, 370, self.display, True, transparency=150)
-                            render_img(self.assets["big-shield"], 750, 370, self.display, True)
-                    
-                    # Store menu (Heart)
-                    if self.store_state == "store_heart":
-                        self.store_clickcooldown = max(0, self.store_clickcooldown - 1)
-                        if render_img(self.assets["+"], 500, 390, self.display, True, True) and self.store_clickcooldown == 0 and self.store_addsub_heart < self.max_heart - self.maxHP:
-                            self.store_addsub_heart += 1
-                            self.store_clickcooldown = 20
-                        if render_img(self.assets["-"], 700, 390, self.display, True, True) and self.store_clickcooldown == 0 and self.store_addsub_heart > 0:
-                            self.store_addsub_heart -= 1
-                            self.store_clickcooldown = 20
-                        render_text(str(self.store_addsub_heart), self.font, "white", 600, 390, self.display, True)
-                        
-                        # Warn player about min and max hearts
-                        if self.store_addsub_heart >= self.max_heart - self.maxHP:
-                            self.store_addsub_heart = self.max_heart - self.maxHP
-                            render_text("Max hearts", self.font, "red", 600, 330, self.display, True)
-                        elif self.store_addsub_heart <= 0:
-                            self.store_addsub_heart = 0
-                            render_text("Min hearts", self.font, "red", 600, 330, self.display, True)
-
-                        if self.player.gold < self.store_addsub_heart * 750:
-                            render_text("Not enough gold", self.font, "red", 600, 440, self.display, True)
-
-                        render_text(str(self.store_addsub_heart * 750), self.font, "black", 600, 490, self.display, True)
-
-                    # Store menu (Speed)
-                    if self.store_state == "store_speed":
-
-                        # make the speed be 1 decimal point
-                        self.store_addsub_speed = round(self.store_addsub_speed, 1)
-                        self.store_clickcooldown = max(0, self.store_clickcooldown - 1)
-                        if render_img(self.assets["+"], 500, 390, self.display, True, True) and self.store_clickcooldown == 0 and self.store_addsub_speed < self.max_speed - self.player.speed:
-                            self.store_addsub_speed = round(self.store_addsub_speed + 0.1, 1)
-                            self.store_clickcooldown = 20
-                        if render_img(self.assets["-"], 700, 390, self.display, True, True) and self.store_clickcooldown == 0 and self.store_addsub_speed > 0:
-                            self.store_addsub_speed = round(self.store_addsub_speed - 0.1, 1)
-                            self.store_clickcooldown = 20
-                        render_text(str(self.store_addsub_speed), self.font, "white", 600, 400, self.display, True)
-                        
-                        # Warn player about min and max speed
-                        if self.store_addsub_speed >= self.max_speed - self.player.speed:
-                            self.store_addsub_speed = self.max_speed - self.player.speed
-                            render_text("Max speed", self.font, "red", 600, 330, self.display, True)
-                        elif self.store_addsub_speed <= 0:
-                            self.store_addsub_speed = 0
-                            render_text("Min speed", self.font, "red", 600, 330, self.display, True)
-
-                        if self.player.gold < self.store_addsub_speed * 5000:
-                            render_text("Not enough gold", self.font, "red", 600, 440, self.display, True)
-
-                        render_text(str(int(self.store_addsub_speed * 5000)), self.font, "black", 600, 490, self.display, True)
-
-                    # Store menu (Shield)
-                    if self.store_state == "store_shield":
-                        self.store_clickcooldown = max(0, self.store_clickcooldown - 1)
-                        if render_img(self.assets["+"], 500, 390, self.display, True, True) and self.store_clickcooldown == 0 and self.store_addsub_shield < self.max_shield - self.player.shield/100:
-                            self.store_addsub_shield += 1
-                            self.store_clickcooldown = 20
-                        if render_img(self.assets["-"], 700, 390, self.display, True, True) and self.store_clickcooldown == 0 and self.store_addsub_shield > 0:
-                            self.store_addsub_shield -= 1
-                            self.store_clickcooldown = 20
-                        render_text("x" + str(round(self.player.shield/100 + self.store_addsub_shield)), self.font, "black", 600, 380, self.display, True)
-                        
-                        # Warn player about min and max shield
-                        if self.store_addsub_shield >= self.max_shield - self.player.shield/100:
-                            self.store_addsub_shield = self.max_shield - self.player.shield/100
-                            render_text("Max shield", self.font, "red", 600, 330, self.display, True)
-                        elif self.store_addsub_shield <= 0:
-                            self.store_addsub_shield = 0
-                            render_text("Min shield", self.font, "red", 600, 330, self.display, True)
-
-                        if self.player.gold < self.store_addsub_shield * 600:
-                            render_text("Not enough gold", self.font, "red", 600, 440, self.display, True)
-
-                        render_text(str(round(self.store_addsub_shield * 600)), self.font, "black", 600, 490, self.display, True)
-                
-                elif self.level_select:
-                    # Check for level_select change
-                    if self.level == "safehouse":
-                        self.level = "level_1"
-                    else:
-                        self.display.blit(self.assets[self.level], (0, 0))
-
-                    # Render the level selection screen
-                    self.level_transition(50)
-                    img = pygame.Surface((1200, 675))
-                    img.fill((0,0,0))
-                    img.set_alpha(150)
-                    self.display.blit(img, (0,0))
-                    self.display.blit(self.assets["level_selection"], (0, 0))
-                    render_text("Level " + self.level.split("_")[1], self.font2, "white", 600, 300, self.display, centered=True)
-                        
     def run(self):
                 
         self.display.blit(self.bg, (0, 0))
         self.render()
 
-        # Example of implementation of code for dialogue
         self.npc_name = self.interact()
         if self.npc_name == "Store":
             self.store = True
@@ -946,6 +828,6 @@ class Play():
         self.core()
         self.transitions()
         self.safehouse()
-        if self.state != "safehouse" and self.level not in ["level_1", "level_2", "level_3"]:
+        if self.state != "safehouse" and self.level not in ["level_1", "level_2", "level_3", "safehouse"]:
             self.level_transition(150, "Level " + str(self.level))
         
