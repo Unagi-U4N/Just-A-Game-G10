97214f00e7d2da90ad12b4184fccd68b4e3c882d
Ivan Chong <1221109063@soffice.mmu.edu.my>
2024-06-10 13:06:46 +0800

    added transition between newgame and game

--- source/music.py
+++ source/music.py
@@ -21,6 +21,7 @@ class Music:
                 if self.music_playing != "":
                     self.game.display.fill((0, 0, 0))
                     render_img(self.game.assets["glitch_blocks"][5], 870, 600, self.game.display, True)
+                    render_text("You can always create a new account if you are bored", pygame.font.Font(self.game.font, 36), "white", 600, 300, self.game.display, centered=True, transparency=255)
                     render_text("Loading...", pygame.font.Font(self.game.font, 50), (255, 255, 255), 1000, 600, self.game.display, True)
                     pygame.mixer.music.fadeout(2000)
                     # pygame.mixer.music.stop()
